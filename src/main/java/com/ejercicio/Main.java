fc393c3b4c2c39e11ba3f8b160cfadcc69599883
unknown <mateoalejandro02a@gmail.com>
2025-02-18 11:36:41 -0500

    Pruebas varias para la carga de datos

--- src/main/java/com/ejercicio/Main.java
+++ src/main/java/com/ejercicio/Main.java
@@ -6,6 +6,12 @@ package com.ejercicio;
 public class Main {
     public static void main(String[] args) {
         InscripcionesPersonas a = new InscripcionesPersonas();
+        Persona person = new Persona();
+        person.setEmail("sust@gmail.com");
+        person.setApellidos("Stallman");
+        person.setNombre("Richard");
+        person.setID((double) 0);
+        a.guardarInformacion(person);
         a.cargarDatos();
         System.out.println(a.getListado());
 
